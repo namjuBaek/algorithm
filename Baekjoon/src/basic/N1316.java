30dfa33a27c6578d564972a11aa10afcdcdf136e
namju <whitenj0125@gmail.com>
2022-01-09 19:04:32 +0900

    baekjoon string N2941 / N1316 commit

---
+++ Baekjoon/src/basic/N1316.java
@@ -0,0 +1,62 @@
+package basic;
+
+/**
+ * 2021.01.09 일
+ * @author bnj
+ * 백준 N1316번 그룹 단어 체커
+ * 
+ * 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 
+ * 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+ * 
+ * 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+ * 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 
+ * 둘째 줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+ */
+
+import java.util.Scanner; 
+import java.util.HashSet;
+
+public class N1316 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int groupCount = 0;
+		int count = scan.nextInt();
+		String[] str = new String[count];
+		
+		//개수만큼 단어 입력받기
+		for (int i=0; i<count; i++) {
+			str[i] = scan.next();
+		}
+		
+		
+		HashSet<String> checkStr = new HashSet<>();	//등장한 알파벳
+		char beforeChr = '\u0000';	//직전 문자
+		boolean isGroup = true;	//그룹단어 여부
+		
+		//케이스 별 반복
+		for (int i=0; i<count; i++) {
+			isGroup = true;
+			checkStr.clear();
+			
+			//그룹단어 체크
+			for (char chr : str[i].toCharArray()) {
+				if (chr != beforeChr && checkStr.contains(String.valueOf(chr))) {
+					isGroup = false;
+					break;
+				}
+				
+				beforeChr = chr;
+				checkStr.add(String.valueOf(chr));
+			}
+			
+			if (isGroup) {
+				groupCount++;
+			}
+		}
+		
+		System.out.println(groupCount);
+	}
+}
---
+++ Baekjoon/src/basic/N2941.java
@@ -0,0 +1,45 @@
+package basic;
+
+/**
+ * 2021.01.09 일
+ * @author bnj
+ * 백준 N2941번 크로아티아 알파벳
+ * 
+ * č	c=
+ * ć	c-
+ * dž	dz=
+ * đ	d-
+ * lj	lj
+ * nj	nj
+ * š	s=
+ * ž	z=
+ * ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+ * dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
+ */
+
+import java.util.Scanner;
+
+public class N2941 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int countWord = 0;
+		
+		String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+		String str = scan.next();
+		
+		for (String alphabet : croatia) {
+			while (str.contains(alphabet)) {
+				str = str.replaceFirst(alphabet, ".");	//.으로 치환하지 않고 아예 없앨경우, 문자가 합쳐져 다른 알파벳이 되는 경우가 발생 ex) nljj -> nj로 바뀌어서 한 문자로 인식됨.
+				countWord++;
+			}
+		}
+		
+		str = str.replace(".", "");
+		countWord += str.length();
+		
+		System.out.println(countWord);		
+	}
+}
